57cf2499fb66d825dd1041884651128972a461e9
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-27 21:05:25 +0500

    updating admin route middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,34 +1,15 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asynchandler.js";
-import jwt from "jsonwebtoken"
-import { User } from "../models/user.model.js";
 
-export const verifyAdmin = asyncHandler(async(req, _, next) => {
+export const verifyAdmin = asyncHandler(async(req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.headers["Authorization"]?.replace("Bearer ", "")
-        
-        // console.log(token);
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
-    
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-        const user = await User.findById(decodedToken?._id).select("-password ")
-    
-        if (!user) {
-            
-            throw new ApiError(401, "Invalid Access Token")
-        }
-        if (!(user.role === "admin")) {
-            // user.role !== "admin"
-             throw new ApiError(403, "Only Admin Can Access this route")
-        }
-    
-        req.user = user;
+       
+        if (!req?.user?.isAdmin) {
+             return res.status(401).json(new ApiError(403, "Only Admin Can Access this route"))
+         }
         next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Accessing Admin Route")
+         return res.status(401).json(new ApiError(401, error?.message || "Accessing Admin Route"))
     }
     
 })
