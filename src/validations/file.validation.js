cbaba3172487a175eeaf439fb1029708d7dfe726
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-08-01 17:53:50 +0500

    multiple files handling

--- src/validations/file.validation.js
+++ src/validations/file.validation.js
@@ -2,6 +2,7 @@ import { defaultAvatar } from "../constants.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"; 
  
  async function validateDiskStorageMultipleFilesAndUploadOnCloudinary (reqFile,name){
+    // work for upload.fields[] in route
 let avatar = defaultAvatar;
 if (reqFile && reqFile?.[name] && reqFile[name]?.[0] && reqFile[name][0]?.path ) {
    const avatarLocalPath = reqFile[name][0]?.path ;
@@ -11,9 +12,14 @@ return avatar
 }
 
 
-async function validateMemoryStorageMultipleFilesAndUploadOnCloudinary(reqFile) {
+async function validateMemoryStorageMultipleFilesAndUploadOnCloudinary(files ,name) {
     let avatar =defaultAvatar;
-    //   not created yet
+    //   not tested yet
+    if (files && files?.[name] && files[name]?.[0] && files[name][0]?.buffer && files[name][0]?.mimetype ) {
+     const b64 = Buffer.from(files[name][0]?.buffer).toString("base64");
+        let dataURI = "data:" + files[name][0]?.mimetype + ";base64," + b64;
+        avatar = await uploadOnCloudinary(dataURI , "memory")
+}
         return avatar;
     
 }
